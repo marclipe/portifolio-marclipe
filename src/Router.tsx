c56480c71f6bfc8d2be54d054d85d4b98b53e65c
MarcLipe <marcos.lima52@fatec.sp.gov.br>
2023-08-24 21:04:37 -0300

    Adding Routes of website

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,10 +1,16 @@
 import { Routes, Route } from 'react-router-dom'
+import { DefaultLayout } from './layouts/DefaultLayout'
+import { Home } from './pages/Home'
+import { About } from './pages/About'
+import { Skills } from './pages/Skills'
 
 export function Router() {
   return (
     <Routes>
-      <Route>
-        <Route></Route>
+      <Route path="/" element={<DefaultLayout />}>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/skills" element={<Skills />}></Route>
       </Route>
     </Routes>
   )
